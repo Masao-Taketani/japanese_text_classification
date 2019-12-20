af3e632022da4feffa362e3f9cd005898e9d8e72
Masao-Taketani <taket003@umn.edu>
2019-12-20 22:24:54 +0900

    modified pretrain script in pretrain.py

--- sentencepiece/bert-japanese/pretrain.py
+++ sentencepiece/bert-japanese/pretrain.py
@@ -6,15 +6,10 @@ def main():
 	input_files = glob("data/wiki/*/*tfrecord")
 	input_files = ','.join(file for file in input_files)
 	print("number of tfrecords: ", len(input_files))
-	subprocess.call(["python "
-			 "src/run_pretraining.py "
-			 "--input_file={} "
-			 "--output_dir={} "
-			 "--do_train=True "
-			 "--do_eval=True "
-			 "--max_seq_length=512 "
-			 "--num_train_steps=1300000".format(
-			 input_files, "model/pretrain")])
+	command = "python src/run_pretraining.py --input_file={} --output_dir={} --do_train=True --do_eval=True --train_batch_size=8 --max_seq_length=512 --num_train_steps=1300000".format(
+	input_files, "model/pretrain")
+
+	subprocess.call(command.split())
 
 
 if __name__ == "__main__":
